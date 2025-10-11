21853dcb0a1b53fa6a4aefebdcb78ef4e60eafe7
Tomas Votruba <tomas.vot@gmail.com>
2025-10-11 20:05:01 +0200

    remove stmts interface test, as focus on bare rector run

--- tests/Rector/MakeNewlineAfterIf/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\RectorCompatTests\Rector\MakeNewlineAfterIfRector;
-
-return RectorConfig::configure()
-    ->withRules([MakeNewlineAfterIfRector::class]);
