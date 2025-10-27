7d2e0a8e74774d5850c4d02a422d8d87028cafda
Tomas Votruba <tomas.vot@gmail.com>
2025-10-27 23:52:31 +0100

    kick off laravel rector, php-parser 5.4 and phpunit 11 test using getArg() calls on CallLike

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\RectorCompatTests\Rector\UseGetArgRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/fixture',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+   ->withRules([UseGetArgRector::class])
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
