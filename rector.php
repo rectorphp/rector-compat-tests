d8423aec89b15196062ebd2354bd0be2e5844211
Tomas Votruba <tomas.vot@gmail.com>
2025-12-05 17:45:30 +0100

    bump deps

--- rector.php
+++ rector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\RectorCompatTests\Rector\UseGetArgRector;
-use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
